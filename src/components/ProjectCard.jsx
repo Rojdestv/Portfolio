37f269aaecdb75e63de69e0727e3c452991c2e5f
Valerie Rojdestvenskaia <vrojdestvenskaia@gmail.com>
2018-11-01 16:16:41 -0700

    mobile functionality update

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import styled from 'react-emotion';
 
 const Wrapper = styled.a`
   width: 100%;
-  ${tw('shadow-lg relative no-underline rounded-lg px-8 py-16 text-white')};
+  ${tw('shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-24 text-white')};
   background: ${props => props.bg};
   transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   &:hover {
@@ -14,7 +14,8 @@ const Wrapper = styled.a`
 `;
 
 const Text = styled.div`
-  ${tw('opacity-75 font-sans')};
+${tw('opacity-75 font-sans text-sm md:text-base')};
+text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 `;
 
 const Title = styled.div`
